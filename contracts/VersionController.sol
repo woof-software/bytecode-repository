0404db642759ef21a8acbad957f2c77b637526e7
philippWOOF <philipp.t@woof.software>
2025-07-08 10:26:25 +0000

    docs: add natspec

--- contracts/VersionController.sol
+++ contracts/VersionController.sol
@@ -18,30 +18,40 @@ contract VersionController is
     EIP712Upgradeable
 {
     using EnumerableSet for EnumerableSet.AddressSet;
-    using EnumerableSet for EnumerableSet.Bytes32Set;
     using Strings for uint256;
 
+    /// @notice Bytecode version typehash.
     bytes32 public constant BYTECODE_VERSION_TYPEHASH =
         keccak256("BytecodeVersion(bytes32 contractType,uint64 major,uint64 minor,uint64 patch,string alternative)");
+    /// @notice Audit report typehash.
     bytes32 public constant AUDIT_REPORT_TYPEHASH = keccak256("AuditReport(bytes32 bytecodeHash,string auditReport)");
+    /// @notice Key Developer role for AccessControl.
     bytes32 public constant KEY_DEVELOPER_ROLE = keccak256("KEY_DEVELOPER_ROLE");
+    /// @notice Sub Developer role for AccessControl.
     bytes32 public constant SUB_DEVELOPER_ROLE = keccak256("SUB_DEVELOPER_ROLE");
+    /// @notice Auditor role for AccessControl.
     bytes32 public constant AUDITOR_ROLE = keccak256("AUDITOR_ROLE");
+    /// @notice A limit of sub developers per key developer.
     uint256 public constant SUB_DEVELOPERS_LIMIT = 3;
-    /// @notice small buffer to account for `SSTORE2` overhead
+    /// @notice small buffer to account for `SSTORE2` overhead.
     uint256 public constant CHUNK_SIZE = 24500;
-
+    /// @notice Stores current key developer for given contract type.
     mapping(bytes32 => address) public contractTypeKeyDeveloper;
+    /// @notice Stores a list of sub developers for given key developer.
     mapping(address => EnumerableSet.AddressSet) private subDevelopers;
+    /// @notice Stores a key developer for given sub developer.
     mapping(address => address) public subToKeyDeveloper;
-
+    /// @notice Stores the latest available version for given contract type.
     mapping(bytes32 => Version) public latestVersions;
+    /// @notice Stores the latest available minor version for given contract type and major version.
     mapping(bytes32 => mapping(uint64 => uint64)) public latestMinor;
+    /// @notice Stores the latest available patch version for given contract type, major and minor versions.
     mapping(bytes32 => mapping(uint64 => mapping(uint64 => uint64))) public latestPatch;
-
+    /// @notice Stores the list of alternative versions for given contract type.
     mapping(bytes32 => VersionWithAlternative[]) private alternativeVersions;
-
+    /// @notice Stores the bytecode information for given bytecode hash.
     mapping(bytes32 => Bytecode) public bytecodes;
+    /// @notice Stores the audits information for given bytecode hash.
     mapping(bytes32 => AuditStatus) private bytecodeAuditStatus;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -56,6 +66,10 @@ contract VersionController is
         _grantRole(DEFAULT_ADMIN_ROLE, _governor);
     }
 
+    /// @notice Validates if the function is called by key developer of corresponding sub developer.
+    /// @dev Additionally checks if developer has an appropriate role.
+    /// @param _contractType A type of contract for which to validate developer.
+    /// @param _developer An address of developer to validate.
     modifier checkDeveloper(bytes32 _contractType, address _developer) {
         address contractTypeKeyDev = contractTypeKeyDeveloper[_contractType];
         if (!hasRole(KEY_DEVELOPER_ROLE, _developer) && !hasRole(SUB_DEVELOPER_ROLE, _developer))
@@ -65,6 +79,8 @@ contract VersionController is
         _;
     }
 
+    /// @notice Validates if initial bytecode for specified contract type is already released.
+    /// @param _contractType A type of contract for which to validate if bytecode is released.
     modifier bytecodeReleased(bytes32 _contractType) {
         if (latestVersions[_contractType].major == 0) revert BytecodeNotReleased(_contractType);
         _;
@@ -74,7 +90,10 @@ contract VersionController is
 
     /// @notice Assigns a new key developer for a certain contract type.
     /// @dev Governor can use this function to initializes contract type and forcibly assign new key developer.
-    /// @dev Key developer can use this functions to transfer developer rights over contracy type to another key developer.
+    /// @dev Key developer can use this functions to transfer developer rights over contract type to another key developer.
+    /// @dev Key developer must already have a KEY_DEVELOPER_ROLE.
+    /// @param _contractType A type of contract to assign developer for.
+    /// @param _keyDeveloper An address of key developer to assign.
     function assignDeveloperForContractType(bytes32 _contractType, address _keyDeveloper) external {
         if (
             !hasRole(DEFAULT_ADMIN_ROLE, msg.sender) &&
@@ -89,6 +108,10 @@ contract VersionController is
 
     /* Upload bytecode functions */
 
+    /// @notice Releases an initial version of the contract type.
+    /// @dev Contract type can only be released once.
+    /// @dev Sets the initial version to "1.0.0".
+    /// @param _bytecodeInput A struct of params necessary to upload the bytecode.
     function releaseBytecode(
         BytecodeInput calldata _bytecodeInput
     ) external checkDeveloper(_bytecodeInput.contractType, msg.sender) {
@@ -101,6 +124,11 @@ contract VersionController is
         latestVersions[_bytecodeInput.contractType] = version.version;
     }
 
+    /// @notice Releases a new major version of the contract type.
+    /// @dev Can only be called of initial bytecode was released for contract type.
+    /// @dev Increments a previously stored major version.
+    /// @dev Updates the latest version of the contract type. For example, if previous latest version was "1.0.3", the new latest version will be set to "2.0.0".
+    /// @param _bytecodeInput A struct of params necessary to upload the bytecode.
     function releaseMajorVersion(
         BytecodeInput calldata _bytecodeInput
     ) external bytecodeReleased(_bytecodeInput.contractType) checkDeveloper(_bytecodeInput.contractType, msg.sender) {
@@ -114,6 +142,14 @@ contract VersionController is
         latestVersions[_bytecodeInput.contractType] = version.version;
     }
 
+    /// @notice Released a new minor version of the contract type for specified major version.
+    /// @dev Can only be called of initial bytecode was released for contract type.
+    /// @dev Specified major version must exist.
+    /// @dev Increments a previously stored minor version of specified major version.
+    /// @dev Updates the latest version of the contract type if specified major version is latest. For example, if latest version is "4.2.1" and passed major
+    /// version is 4, the new latest version will be set to "4.3.0".
+    /// @param _bytecodeInput A struct of params necessary to upload the bytecode.
+    /// @param _major A major version for which to release new minor version.
     function releaseMinorVersion(
         BytecodeInput calldata _bytecodeInput,
         uint64 _major
@@ -133,6 +169,14 @@ contract VersionController is
         }
     }
 
+    /// @notice Releases a new patch version of the contract type for specified major and minor versions.
+    /// @dev Specified major and minor versions must exist.
+    /// @dev Increments a previously stored patch version for specified major and minor versions.
+    /// @dev Updates the latest version of the contract type if specified major and minor versions are latest. For example, if latest version is "4.3.2", passed major
+    /// version is 4 and minor is 3, the new latest version will be set to "4.3.3".
+    /// @param _bytecodeInput A struct of params necessary to upload the bytecode.
+    /// @param _major A major version for which to release new patch version.
+    /// @param _minor A minor version for which to release new patch version.
     function releasePatchVersion(
         BytecodeInput calldata _bytecodeInput,
         uint64 _major,
@@ -153,18 +197,17 @@ contract VersionController is
         if (_major == latestVersion.major && _minor == latestVersion.minor) latestVersion.patch = version.version.patch;
     }
 
+    /// @notice Releases an alternative version for specified contract type and version.
+    /// @dev Specified version, for which alternative is released, must exist.
+    /// @param _bytecodeInput A struct of params necessary to upload the bytecode.
+    /// @param _version A struct of params containing version for which to release alternative version and an alternative version to release.
     function releaseAlternativeVersion(
         BytecodeInput calldata _bytecodeInput,
         VersionWithAlternative calldata _version
     ) external checkDeveloper(_bytecodeInput.contractType, msg.sender) {
-        if (_version.version.major > latestVersions[_bytecodeInput.contractType].major)
-            revert NonExistingMajorVersion(_bytecodeInput.contractType, _version.version.major);
-        if (_version.version.minor > latestMinor[_bytecodeInput.contractType][_version.version.major])
-            revert NonExistingMinorVersion(_bytecodeInput.contractType, _version.version.major, _version.version.minor);
-        if (
-            _version.version.patch >
-            latestPatch[_bytecodeInput.contractType][_version.version.major][_version.version.minor]
-        ) revert NonExistingPatch(_bytecodeInput.contractType, _version.version);
+        VersionWithAlternative memory coreVersion = VersionWithAlternative(_version.version, "");
+        if (!versionExists(computeBytecodeHash(_bytecodeInput.contractType, coreVersion)))
+            revert NonExistingVersion(_bytecodeInput.contractType, coreVersion);
         address keyDeveloper = getKeyDeveloper(msg.sender);
         _uploadBytecode(_bytecodeInput, keyDeveloper, _version);
 
@@ -173,6 +216,14 @@ contract VersionController is
 
     /* Auditor functions */
 
+    /// @notice Uploads an audit report for specified bytecode.
+    /// @dev Can only be called by the auditors.
+    /// @dev Uploaded audit report must be unique for specified bytecode version.
+    /// @dev Auditor should provide a signature following the https://eips.ethereum.org/EIPS/eip-712.
+    /// @dev Caller must be the author of the signature.
+    /// @param _bytecodeVersion A version of bytecode to verify.
+    /// @param _auditReport a URL to the audit report.
+    /// @param _signature a signature signed by auditor verifying that the auditor intents to approve the bytecode.
     function verifyBytecode(
         BytecodeVersion calldata _bytecodeVersion,
         string calldata _auditReport,
@@ -219,6 +270,10 @@ contract VersionController is
 
     /* View functions */
 
+    /// @notice Computes a bytecode hash for specified contract type and its version.
+    /// @param _contractType A type of contract for which to compute hash.
+    /// @param _version A version of specified contract type for which to compute hash.
+    /// @return Hash of specified bytecode.
     function computeBytecodeHash(
         bytes32 _contractType,
         VersionWithAlternative memory _version
@@ -236,26 +291,49 @@ contract VersionController is
             );
     }
 
+    /// @notice Computes an audit report hash.
+    /// @param _bytecodeHash A hash of bytecode version for which the audit report hash is computed.
+    /// @param _auditReport a URL of the audit report.
+    /// @return Hash of specified audit report.
     function computeAuditReportHash(bytes32 _bytecodeHash, string calldata _auditReport) public pure returns (bytes32) {
         return keccak256(abi.encode(AUDIT_REPORT_TYPEHASH, _bytecodeHash, keccak256(bytes(_auditReport))));
     }
 
+    /// @notice Returns the key developer for given account.
+    /// @dev Returns given account address if it has a key developer role.
+    /// @dev Returns a key developer of a given account if the given account is sub developer.
+    /// @dev Returns zero address if given account is not a developer.
+    /// @param _account Address for which to return a key developer.
+    /// @return Address of key developer.
     function getKeyDeveloper(address _account) public view returns (address) {
         return hasRole(KEY_DEVELOPER_ROLE, _account) ? _account : subToKeyDeveloper[_account];
     }
 
+    /// @notice Returns a list of sub developers for given key developer.
+    /// @dev Returns empty array if given account is not a key developer.
+    /// @param _keyDeveloper Address of key developer.
+    /// @return Array containing the list of sub developers for given key developer.
     function getSubDevsForKeyDeveloper(address _keyDeveloper) public view returns (address[] memory) {
         return subDevelopers[_keyDeveloper].values();
     }
 
+    /// @notice Validates if given bytecode version is verified by at least one auditor.
+    /// @param _version Version of bytecode to validate.
+    /// @return A boolean flag indicating if bytecode is verified. True if verified, false otherwise.
     function isBytecodeVerified(BytecodeVersion calldata _version) public view returns (bool) {
         return bytecodeAuditStatus[computeBytecodeHash(_version.contractType, _version.version)].verified;
     }
 
+    /// @notice Returns the latest version for given bytecode in human-readable format.
+    /// @param _contractType A type of contract for which to return latest version.
+    /// @return Latest version for given contract type. E.g. "2.3.0".
     function getLatestVersion(bytes32 _contractType) external view returns (string memory) {
         return _versionToStr(VersionWithAlternative(latestVersions[_contractType], ""));
     }
 
+    /// @notice Returns a list of auditors who have approved the specified bytecode.
+    /// @param _version Bytecode version for which to return a lits of auditors.
+    /// @return Array containing all the auditors of specified bytecode. Empty if none of the auditors have approved the bytecode yet.
     function getAuditorsForBytecodeVersion(BytecodeVersion calldata _version) external view returns (address[] memory) {
         return bytecodeAuditStatus[computeBytecodeHash(_version.contractType, _version.version)].auditors;
     }
@@ -269,26 +347,43 @@ contract VersionController is
         return bytecodeAuditStatus[computeBytecodeHash(_version.contractType, _version.version)].auditReports[_auditor];
     }
 
+    /// @notice Validates if a specified bytecode version exists based on struct with contract type and version.
+    /// @param _version A bytecode version to check.
+    /// @return A boolean flag indicating if the version exists. True if exists, false otherwise.
     function versionExists(BytecodeVersion calldata _version) external view returns (bool) {
         return versionExists(computeBytecodeHash(_version.contractType, _version.version));
     }
 
+    /// @notice Validates if a specified bytecode version exists based on bytecode hash.
+    /// @param _bytecodeHash A bytecode hash to check.
+    /// @return A boolean flag indicating if the version exists. True if exists, false otherwise.
     function versionExists(bytes32 _bytecodeHash) public view returns (bool) {
         return bytecodes[_bytecodeHash].author != address(0);
     }
 
+    /// @notice Returns a verified bytecode of a specified contract type and version.
     /// @dev Throws and error if bytecode is not verified by at least one auditor.
+    /// @param _version A bytecode version for which to return bytecode.
+    /// @return A bytecode of specified contract type and version.
     function getVerifiedBytecode(BytecodeVersion calldata _version) external view returns (bytes memory) {
         if (!isBytecodeVerified(_version)) revert BytecodeNotVerified(_version);
         return _readInitCode(bytecodes[computeBytecodeHash(_version.contractType, _version.version)].initCodePtrs);
     }
 
+    /// @notice Returns all alternative versions for given contract type.
+    /// @param _contractType A type of contract for which to return alternative versions.
+    /// @return Array containing all the alternative versions of given contract type.
     function getAllAlternativeVersions(bytes32 _contractType) external view returns (VersionWithAlternative[] memory) {
         return alternativeVersions[_contractType];
     }
 
     /* Internal helpers */
 
+    /// @notice Stores the bytecode of given contract type and version.
+    /// @dev Validates if bytecode is not already uploaded for given contract type and version.
+    /// @param _bytecodeInput A struct of params containing info about bytecode.
+    /// @param _keyDeveloper Address of key developer for given contract type at the time of uploading the bytecode.
+    /// @param _version A new version of bytecode for which it is uploaded.
     function _uploadBytecode(
         BytecodeInput calldata _bytecodeInput,
         address _keyDeveloper,
@@ -309,6 +404,11 @@ contract VersionController is
         emit BytecodeUploaded(_bytecodeInput.contractType, _version.version);
     }
 
+    /// @notice Writes the bytecode in the storage.
+    /// @dev Utilizes SSTORE2 for storing the bytecode.
+    /// @dev Bytecode must not be empty.
+    /// @param _initCode Bytecode to store.
+    /// @return An array of pointer addresses at which parts of bytecode was stored.
     function _writeInitCode(bytes calldata _initCode) internal returns (address[] memory) {
         if (_initCode.length == 0) revert InitCodeIsEmpty();
         uint256 len = (_initCode.length - 1) / CHUNK_SIZE + 1;
@@ -322,6 +422,9 @@ contract VersionController is
         return initCodePointers;
     }
 
+    /// @notice Returns the bytecode stored at given address pointers.
+    /// @param _initCodePtrs Address pointers at which parts of the bytecode is stored.
+    /// @return Bytecode stored at the given pointers.
     function _readInitCode(address[] memory _initCodePtrs) internal view returns (bytes memory) {
         bytes memory initCode;
         for (uint256 i; i < _initCodePtrs.length; ++i) {
@@ -330,6 +433,9 @@ contract VersionController is
         return initCode;
     }
 
+    /// @notice Returns given version in human-readable format. E.g. "4.3.0".
+    /// @param _version Version of the bytecode.
+    /// @return A string containing the version.
     function _versionToStr(VersionWithAlternative memory _version) internal pure returns (string memory) {
         return
             string.concat(
--- contracts/interfaces/IVersionController.sol
+++ contracts/interfaces/IVersionController.sol
@@ -23,33 +23,68 @@ interface IVersionController {
     error InvalidAuditor(address _author);
     error BytecodeNotVerified(BytecodeVersion _version);
     error NotDeveloper(address _account);
-    error NonExistingPatch(bytes32 _contractType, Version _version);
     error WrongKeyDeveloper(address account, address _subDeveloper);
     error VersionAlreadyExists(bytes32 _contractType, VersionWithAlternative _version);
+    error NonExistingVersion(bytes32 _contractType, VersionWithAlternative _version);
 
+    /**
+     * @notice Represents a version.
+     * @dev Fields:
+     * - `major`: major version
+     * - `minor`: minor version
+     * - `patch`: patch version
+     */
     struct Version {
         uint64 major;
         uint64 minor;
         uint64 patch;
     }
 
+    /**
+     * @notice Represents a version with alternative.
+     * @dev Fields:
+     * - `version`: core version
+     * - `alternative`: alternative version
+     */
     struct VersionWithAlternative {
         Version version;
         string alternative;
     }
 
+    /**
+     * @notice Represents params necessary for releasing bytecode.
+     * @dev Fields:
+     * - `contractType`: a type of contract to release
+     * - `initCode`: bytecode to upload
+     * - `sourceURL`: link to repository containing the source code of bytecode
+     */
     struct BytecodeInput {
         bytes32 contractType;
         bytes initCode;
         string sourceURL;
     }
 
+    /**
+     * @notice Represents information about audit status of bytecode.
+     * @dev Fields:
+     * - `auditors`: array of auditors who have approved the bytecode
+     * - `verified`: a boolean flag indicating if the bytecode has been approved by at least a single auditor
+     * - `auditReports`: a mapping containing audit report URL of a given auditor
+     */
     struct AuditStatus {
         address[] auditors;
         bool verified;
         mapping(address => string) auditReports;
     }
 
+    /**
+     * @notice Represents information about stored bytecode.
+     * @dev Fields:
+     * - `contractType`: a type of contract of given bytecode
+     * - `initCodePtrs`: address pointers at which parts of bytecode are stored
+     * - `sourceURL`: link to repository where source code of bytecode is stored
+     * - `author`: address of the author of bytecode
+     */
     struct Bytecode {
         bytes32 contractType;
         address[] initCodePtrs;
@@ -57,8 +92,74 @@ interface IVersionController {
         address author;
     }
 
+    /**
+     * @notice Represents a contract type with its version
+     * @dev Fields:
+     * - `contractType`: a type of contract
+     * - `version`: version with alternative for given contract type
+     */
     struct BytecodeVersion {
         bytes32 contractType;
         VersionWithAlternative version;
     }
+
+    // Governor
+    function assignDeveloperForContractType(bytes32 _contractType, address _keyDeveloper) external;
+
+    // Bytecode Upload
+    function releaseBytecode(BytecodeInput calldata _bytecodeInput) external;
+
+    function releaseMajorVersion(BytecodeInput calldata _bytecodeInput) external;
+
+    function releaseMinorVersion(BytecodeInput calldata _bytecodeInput, uint64 _major) external;
+
+    function releasePatchVersion(BytecodeInput calldata _bytecodeInput, uint64 _major, uint64 _minor) external;
+
+    function releaseAlternativeVersion(
+        BytecodeInput calldata _bytecodeInput,
+        VersionWithAlternative calldata _version
+    ) external;
+
+    // Auditor
+    function verifyBytecode(
+        BytecodeVersion calldata _bytecodeVersion,
+        string calldata _auditReport,
+        bytes calldata _signature
+    ) external;
+
+    // Key Developer
+    function addSubDeveloper(address _subDeveloper) external;
+
+    function removeSubDeveloper(address _subDeveloper) external;
+
+    // View
+    function computeBytecodeHash(
+        bytes32 _contractType,
+        VersionWithAlternative memory _version
+    ) external pure returns (bytes32);
+
+    function computeAuditReportHash(
+        bytes32 _bytecodeHash,
+        string calldata _auditReport
+    ) external pure returns (bytes32);
+
+    function getKeyDeveloper(address _account) external view returns (address);
+
+    function getSubDevsForKeyDeveloper(address _keyDeveloper) external view returns (address[] memory);
+
+    function isBytecodeVerified(BytecodeVersion calldata _version) external view returns (bool);
+
+    function getLatestVersion(bytes32 _contractType) external view returns (string memory);
+
+    function getAuditorsForBytecodeVersion(BytecodeVersion calldata _version) external view returns (address[] memory);
+
+    function getAuditReport(BytecodeVersion calldata _version, address _auditor) external view returns (string memory);
+
+    function versionExists(BytecodeVersion calldata _version) external view returns (bool);
+
+    function versionExists(bytes32 _bytecodeHash) external view returns (bool);
+
+    function getVerifiedBytecode(BytecodeVersion calldata _version) external view returns (bytes memory);
+
+    function getAllAlternativeVersions(bytes32 _contractType) external view returns (VersionWithAlternative[] memory);
 }
