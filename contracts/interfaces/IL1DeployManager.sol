37b8ce5c3115d606bbcdf9094dd505402b64ad82
philippWOOF <philipp.t@woof.software>
2025-09-12 13:40:43 +0000

    chore: add license

--- contracts/interfaces/IL1DeployManager.sol
+++ contracts/interfaces/IL1DeployManager.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
 import { IVersionController } from "./IVersionController.sol";
--- contracts/libraries/BytecodeStore.sol
+++ contracts/libraries/BytecodeStore.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
 import { SSTORE2 } from "solady/src/utils/SSTORE2.sol";
